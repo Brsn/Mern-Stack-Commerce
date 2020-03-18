328459d002fc0fdce97daeec8c89a3aab94db8fe
Bronson Brown <Brsn90@icloud.com>
2020-03-17 20:07:15 -0600

    3/17/2020

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+    .then(console.log("Connected"))
+    .catch(error => handleError(error));
+
+app.get('/', (req, res) => {
+    res.send('hello from node');
+});
+
+const port = process.env.PORT || 8000;
+
+
+app.listen(port, () => {
+    console.log(`App is running on ${port}`);
+});
+
