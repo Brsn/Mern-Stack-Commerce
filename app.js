c4cfa73cfbec26eaaa644b972a7b023b28de4dd6
Bronson Brown <Brsn90@icloud.com>
2020-04-07 17:27:15 -0600

    4/7/2020

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+//saving user creditentals to cookies
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 require('dotenv').config();
 
+//db
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -12,6 +17,9 @@ mongoose.connect(process.env.DATABASE, {
     .then(() => console.log('DB Connected'));
 
 //routes middleware
+app.use(morgan('dev'));
+//gets json data from the app body
+app.use(bodyParser.json());
 //app.use(userRoutes);
 app.use("/api", userRoutes);
 
