9e7c741be307f5d4844d9acbc7c30dd5471deb85
Bronson Brown <Brsn90@icloud.com>
2020-04-09 22:37:57 -0600

    4/9/2020

--- controllers/user.js
+++ controllers/user.js
@@ -28,5 +28,17 @@ exports.signin = (req, res) => {
                 err: 'User with that email does not exists'
             });
         }
+        //If user is found make sure credentials match.
+
+        //generate a signed token with user id and secret
+
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET)
+
+
+        //persist the token as 't' in cookie with expiry date
+        res.cookie('t', token, { expire: new Date() + 9999 })
+        //return response with user and token to frontend client
+        const { _id, name, email, role } = user
+        return res.json({ token, user: { _id, email, name, role } })
     });
-}
+};
