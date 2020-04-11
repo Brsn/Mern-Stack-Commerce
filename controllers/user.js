4cb22c54fa69ec470ab82f2602f8bc2767009dbd
Bronson Brown <Brsn90@icloud.com>
2020-04-10 23:07:00 -0600

    4/10/2020

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 const { errorHandler } = require('../helpers/dbErrorHandler');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
-
+require("dotenv").config();
 
 exports.signup = (req, res) => {
     //console.log("req.body", req.body);
@@ -10,7 +10,7 @@ exports.signup = (req, res) => {
     user.save((err, user) => {
         if (err) {
             return res.status(400).json({
-                err: errorHandler(err)
+                error: errorHandler(err)
             });
         }
         res.json({
@@ -21,15 +21,21 @@ exports.signup = (req, res) => {
 
 exports.signin = (req, res) => {
     //find the user via email
-    const { email, password } = req.body
+    const { email, password } = req.body;
+
+
     User.findOne({ email }, (err, user) => {
         if (err || !user) {
             return res.status(400).json({
-                err: 'User with that email does not exists'
+                error: 'User with that email does not exists'
             });
         }
         //If user is found make sure credentials match.
-
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'Email and password dont match'
+            });
+        }
         //generate a signed token with user id and secret
 
         const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET)
@@ -38,7 +44,12 @@ exports.signin = (req, res) => {
         //persist the token as 't' in cookie with expiry date
         res.cookie('t', token, { expire: new Date() + 9999 })
         //return response with user and token to frontend client
-        const { _id, name, email, role } = user
+        const { _id, name, email, role } = user;
         return res.json({ token, user: { _id, email, name, role } })
     });
 };
+
+exports.signout = (req, res) => {
+    res.clearCookie('t');
+    res.json({ message: "Signout Successful" });
+}
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //make sure you require the controller
-const { signup, signin } = require('../controllers/user');
+const { signup, signin, signout } = require('../controllers/user');
 const { userSignupValidator } = require("../validator");
 
 /* router.get('/', (req, res) => {
@@ -11,4 +11,5 @@ const { userSignupValidator } = require("../validator");
 
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
+router.get('/signout', signout);
 module.exports = router;
