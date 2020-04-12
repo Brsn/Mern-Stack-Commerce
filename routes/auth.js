cea407d3b128825bc3135f5e63a93ec688d87c06
Bronson Brown <Brsn90@icloud.com>
2020-04-11 23:48:20 -0600

    4/11/2020

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+//make sure you require the controller
+const { signup, signin, signout, requireSignin } = require('../controllers/auth');
+const { userSignupValidator } = require("../validator");
+
+/* router.get('/', (req, res) => {
+    res.send("hello world");
+}); */
+
+router.post('/signup', userSignupValidator, signup);
+router.post('/signin', signin);
+router.get('/signout', signout);
+
+
+
+
+module.exports = router;
