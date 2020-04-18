76e212e95bac59b59402e4ac4a1df8db5149a742
Bronson Brown <Brsn90@icloud.com>
2020-04-17 22:49:26 -0600

    4/17/2020

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const { requireSignin } = require("../controllers/auth");
 //make sure you require the controller
 const { userByID } = require('../controllers/user');
 
+router.get('/secret/:userId', require, (req, res) => {
+    res.json({
+        user: req.profile
+    });
+});
 
 /* router.get('/', (req, res) => {
     res.send("hello world");
