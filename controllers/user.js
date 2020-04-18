e332cd4f42f3b2c413227a2d3406619a16869770
Bronson Brown <Brsn90@icloud.com>
2020-04-17 23:57:23 -0600

    4/17/2020

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,14 @@
 const User = require('../models/user');
+const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
         if (err || !user) {
             return res.status(400).json({
                 error: 'User not found'
-            })
+            });
         }
-        //add user object to the request
-        req.profile = user
-        //middleware won't get stuck with next();
+        req.profile = user;
         next();
-    })
-}
+    });
+};
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 //make sure you require the controller
-const { userByID } = require('../controllers/user');
+const { userById } = require('../controllers/user');
 
 router.get('/secret/:userId', requireSignin, isAuth, (req, res) => {
     res.json({
@@ -15,8 +15,6 @@ router.get('/secret/:userId', requireSignin, isAuth, (req, res) => {
     res.send("hello world");
 }); */
 
-router.param('userId', userById)
-
-
+router.param('userId', userById);
 
 module.exports = router;
